51a49f6cab25e949facb6670b494510127c5f8d3
Depo0 <arsenhruho@gmail.com>
2024-04-29 00:28:19 +0300

    change

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
 
-    const totalPrice =  price * deliveryFee;
+    const totalPrice =  price + deliveryFee;
 
     const message = `Shipping to ${country} will cost ${totalPrice} credits`
 
